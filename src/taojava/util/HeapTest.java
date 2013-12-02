d23c9a3258c015377753ef5186e59af59fa58f37
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-12-01 19:45:50 -0600

    Add blank test of the heap class.

---
+++ src/taojava/util/HeapTest.java
@@ -0,0 +1,16 @@
+package taojava.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Template for an eventual unit test.
+ */
+public class HeapTest {
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    } // test()
+} // class HeapTest
