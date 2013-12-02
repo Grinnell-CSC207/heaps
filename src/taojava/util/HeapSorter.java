8722ef75dc5e99f305aa9ad10efbd4ecb38b1ad3
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-12-01 19:36:18 -0600

    Add a template for a heap sort algorithm.

---
+++ src/taojava/util/HeapSorter.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+
+/**
+ * Sort using heap sort.
+ */
+public class HeapSorter {
+    // +----------------+--------------------------------------------------
+    // | Static Methods |
+    // +----------------+
+
+    /**
+     * Sort an array.
+     */
+    public static <T> void sort(T[] values, Comparator<T> order) {
+        // STUB
+    } // sort(T[], Comparator<T>)
+} // class HeapSorter
